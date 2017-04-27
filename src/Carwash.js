8c5c80c1897b7f9e8c6f6a813e5794915e5ac118
vgutsu <vgutsu@shakuro.com>
2017-04-27 18:28:44 +0300

    added items added onscroll listener

--- src/Carwash.js
+++ src/Carwash.js
@@ -5,7 +5,18 @@
  */
 
 import React, { Component } from 'react';
+var moment= require('moment');
 var minSecMs= require ('minutes-seconds-milliseconds');
+var background_street = require('./img/background_street.png');
+var icon_clear_sky=require('./img/icon_clear_sky.png');
+var icon_clouds=require('./img/icon_clouds.png');
+var icon_few_clouds=require('./img/icon_few_clouds.png');
+var icon_thunderstorm=require('./img/icon_thunderstorm.png');
+var icon_rain= require('./img/icon_rain.png');
+var icon_snow=require('./img/icon_snow.png');
+var icon_mist=require('./img/icon_mist.png');
+var icon_mist=require('./img/icon_mist.png');
+var car_wheel = require('./img/car_wheel.png')
 import {	
 TouchableOpacity, 
 AppRegistry,
@@ -14,107 +25,180 @@ Text,Dimensions,
 View,ListView,ScrollView,Image 
 } from 'react-native';
 
-var laps=[1,1,1,1,1,1,1,1,1,1]
 
 export default class Carwash extends Component {
 constructor(props) {
         super(props);
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.initialState = {
-            dataSource:ds.cloneWithRows(laps),
+            dataSource:ds.cloneWithRows([]),
         };
-       this.state = this.initialState;
-
+       	this.state = this.initialState;
+		this.getWeatherApi();
     }
-
+    		 // 
   render() {
     return (
     	<View style={styles.container}>
-	    	    	   	
-    		{this.showCarView()}
-    		{this.showListViewWithBackground()}
-			<View style={{height:75,flexDirection:'row'}}>
-				{this.cleanButton()}
-				{this.normalButton()}    	
-				{this.dirtyButton()}
-			</View>	
+			<View style={{marginLeft:16}}>
+				<Text style={{fontSize:14,color:'#ffffff',opacity:0.5}}>PLANET</Text>				
+				<Text style={{fontSize:25,color:'white'}}>Earth</Text>
+			</View>
+
+			<View style={{top:50,alignItems:'center',justifyContent:'center'}}>
+				<Text style={{textAlign:'center',fontSize:25,color:'#ffffff'}}>We recommend you {'\n'} to wash your car on:</Text> 
+				<Text style={{textAlign:'center',fontSize:25,color:'#ffffff', textDecorationLine: 'underline' }} >Thursday, 22</Text>
+
+				
+			</View>
+			{this.showListViewWithBackground()}
+			{this.showCarView()}
+			{this.showCarStateButtonView()}
+		
 	   	</View>
     );
   }
- 
+
+ 	showCarStateButtonView(){
+ 		return <View style={styles.carViewStateStyle}>
+				{this.cleanButton()}
+				{this.normalButton()}    	
+				{this.dirtyButton()}
+			</View>
+ 	}
 	showCarView(){
-		return <View 
-			style={{justifyContent:'center',alignItems:'center'}}>
+		return <View
+		style={{
+			position: 'absolute',
+			right:0,
+			left:0,
+			bottom:175,
+			alignItems:'center'
+		}}
+			>
 		 	<Image 
 				style={styles.carWheelStyle}
-    			source={require('./img/car_wheel.png')}/>
+    			source={car_wheel}/>
     	</View>
 	}
 
 	
 
 	cleanButton(){
-		return <View style={{flex:1,backgroundColor:'orange'}}></View>
+		return <View style={styles.buttonStyle}><Text style={styles.textButtonStyle}>CLEAN</Text></View>
 	}
 
 	normalButton(){
-		return <View style={{flex:1,backgroundColor:'red'}}></View>
+		return <View style={styles.buttonStyle}><Text style={styles.textButtonStyle}>NORMAL</Text></View>
 	}
 
 	dirtyButton(){
-		return <View style={{flex:1,backgroundColor:'blue'}}></View>
+		return <View style={styles.buttonStyle}><Text style={styles.textButtonStyle}>DIRTY</Text></View>
+	}
+	
+	handleScroll(event: Object) {
+	  console.log(event.nativeEvent.contentOffset.x);
 	}
 
-
 	showListViewWithBackground(){
-		let width = Dimensions.get('window').width
+		return <ScrollView
+			onScroll={this.handleScroll} scrollEventThrottle={10} 
+			showsHorizontalScrollIndicator={false}
+	        horizontal={true}>
+		 	<View style={styles.scrollableView}>
+					<Image style={styles.backgroundStyle}
+							source={background_street}/>
 
-		return <View style={{ flex:1, backgroundColor: 'transparent' }}>
-		<ScrollView 
-	        showsHorizontalScrollIndicator={false}
-	        horizontal={true}
-	        style={{backgroundColor:'#165488'}}>
-		 	
-		 	<View>
-				<Image
-		        	style={{position: 'absolute', right:0, left:0,bottom:0}}
-					source={require('./img/background_street.png')}/>
-					<ListView style={styles.listViewBorder}
+			 		<View style={styles.horizontalListView}>
+						<ListView 
 					  	    showsHorizontalScrollIndicator={false}
 				  			horizontal={true}
 							enableEmptySections={true}
 					        dataSource={this.state.dataSource}
 					        renderRow={(rowData,sectionID,rowID) => 
-										<View style={styles.rowStyle}>
-							    			<Image 
-							    				style={styles.imageRowStyle}
-							    				source={require('./img/icon_clear_sky.png')}/>
-							  	  			<Text style={{fontSize:10,color:'#ffffff'}}>
-													THU, 22
-							  				</Text>
-							  			</View>
-						    			}	
-						        	/>	 
+										<View style={styles.itemStyle}>
+											{this.getItemWeatherImage(rowData.weather[0].icon)}
+								    		{this.getItemWeatherText(rowID,rowData.dt)}
+								    	</View>
+					    			}	
+						/>	 
+					</View>
 		 	</View>
-				</ScrollView> 
-      </View>
-	}
-	
+		</ScrollView> 
+ 	}
+
+ 	getItemWeatherImage(icon){
+ 		var drawable = "";
+		 switch (icon) {
+            case '01d':
+            		drawable=icon_clear_sky;
+                break;
+            case '02d':
+            		drawable=icon_few_clouds;
+                break;
+            case '03d':
+            case '04d':
+            		drawable=icon_clouds;
+                break;
+			case '09d':
+            case '10d':
+            		drawable=icon_rain;
+                break;
+
+			case '11d':
+            	drawable=icon_thunderstorm;
+            	break;
+			
+			case '13d':
+            	drawable=icon_snow;
+            break;
+            
+            case '50d':
+            	drawable=icon_mist;
+            break;
+
+        }
+		return <Image 
+			style={styles.imageItemStyle}
+			source={drawable}
+		/>
+ 	}
+		
+	getItemWeatherText(position, miliseconds){
+		var simpleDate ='NOW';
+		if(position!=0){
+			simpleDate = moment(miliseconds*1000).format("ddd, D").toUpperCase();
+		}
+		return <Text style={{fontSize:10,color:'#ffffff'}}>
+					{simpleDate}
+				</Text>
+ 	}
 	handleLapPress = () => {
 			laps=laps.concat(2);
 			this.setState({
 				dataSource: this.state.dataSource.cloneWithRows(laps),
 			});	
 	}
+	async getWeatherApi() {
+	    try {
+	    let response = await fetch('http://api.openweathermap.org/data/2.5/forecast/daily?&appid=f98dbbd0a843d201a2a5b407d984b04e&cnt=15&lat=44.6166&lon=33.5254');
+	    let responseJson = await response.json();
+	      // return responseJson.list;
+	    this.setState({
+				dataSource: this.state.dataSource.cloneWithRows(responseJson.list),
+			});
+	    } catch(error) {
+	      console.error(error);
+	    }
+	}
 }
 
 var styles = StyleSheet.create({
 	container:{
 		backgroundColor:'#1072A3',
-		alignItems:'baseline',
 		flex:1
 	},
-	rowStyle:{
+	itemStyle:{
 		marginRight:1,
 		width:72,
 		height:95,
@@ -122,44 +206,53 @@ var styles = StyleSheet.create({
 		justifyContent:'center',
 		alignItems:'center'
 		},
-	imageRowStyle:{
+	imageItemStyle:{
 		marginBottom:10,
-		width:50,
-		height:50
+		width:40,
+		resizeMode: 'contain',
+		height:40
 	},
 	carWheelStyle:{
-
 		width:200,
-		height:69,
+		resizeMode: 'contain',
+		height:70,
 		marginLeft:5,
 		marginRight:5
 	},
 
-	listViewBorder:{
+	buttonStyle:{
+		flex:1,
+		alignItems:'center',
+		justifyContent:'center'
+	},
+	
+	textButtonStyle:{
+		fontSize:16,
+		color:"white",
+	},
+
+	horizontalListView:{
+		height:95,
 		borderTopColor:'red',
   		borderTopWidth:5
 	},	
-
-	topLeft:{
-    position: 'absolute',
-    left: 0,
-    top: 0
-  },
-    topRight:{
-    position: 'absolute',
-    right: 0,
-    top: 0
-  },
-    bottomLeft:{
-    position: 'absolute',
-    left: 0,
-    bottom: 0
-  },
-    bottomRight:{
-    position: 'absolute',
-     right: 0,
-    bottom: 0
-  }
+	carViewStateStyle:{
+		height:75,
+		flexDirection:'row'
+	},
+ 	
+ 	backgroundStyle:{
+		width:15*72+15*1,
+ 		flex:1,
+ 		height: undefined,
+ 		justifyContent: 'center',
+    	alignItems: 'center',
+    	resizeMode:'cover'
+ 	},
+ 	scrollableView:{
+ 		alignItems:'flex-end',
+ 		justifyContent:'flex-end'
+ 	}
 });
 
 AppRegistry.registerComponent('FirstApp', () => Carwash);
